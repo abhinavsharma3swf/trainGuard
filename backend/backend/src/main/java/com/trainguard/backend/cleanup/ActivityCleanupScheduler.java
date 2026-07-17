1767cd3f38d02562c3a5b7d5809a38946196dcd3
abhinavsharma3swf <abhinav.sharma3@hotmail.com>
2026-07-16 19:47:04 -0500

    config fix

--- backend/backend/src/main/java/com/trainguard/backend/cleanup/ActivityCleanupScheduler.java
+++ backend/backend/src/main/java/com/trainguard/backend/cleanup/ActivityCleanupScheduler.java
@@ -3,14 +3,16 @@ package com.trainguard.backend.cleanup;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+//@Component
 @RequiredArgsConstructor
 @ConfigurationProperties(prefix = "activity.cleanup")
 public class ActivityCleanupScheduler {
 
     private final ActivityCleanupService cleanupService;
 
-    @Scheduled(cron = "0 35 7 * * *", zone = "America/Chicago")
+    @Scheduled(cron = "0 53 19 * * *", zone = "America/Chicago")
     public void deleteExpiredActivities() {
         cleanupService.deleteActivitiesOlderThanSevenDays();
     }
