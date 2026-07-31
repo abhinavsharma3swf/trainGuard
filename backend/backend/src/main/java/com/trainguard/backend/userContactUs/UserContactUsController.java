a09c32b1b99c4901d4742f0b2a50f0cac8843ad9
abhinavsharma3swf <abhinav.sharma3@hotmail.com>
2026-07-31 14:23:25 -0500

    We will see if it fix

--- backend/backend/src/main/java/com/trainguard/backend/userContactUs/UserContactUsController.java
+++ backend/backend/src/main/java/com/trainguard/backend/userContactUs/UserContactUsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/contactUs")
+@RequestMapping("/api/contactUs")
 @RequiredArgsConstructor
 public class UserContactUsController {
 
